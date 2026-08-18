4f95f718405ec25f76bf89f3354c3a596cdcfcba
Stéphane <sophiemartel1206@gmail.com>
2026-08-18 13:30:30 +0200

    faute

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Wardy FBI",
+  title: "Wardy BIP",
   description: "Vérifiez les profils signalés et consultez les informations disponibles.",
   openGraph: {
     title: "Wardy FBI",
