06eebab41c239a682cb590b9dec4d6b9642e14c2
Stéphane <sophiemartel1206@gmail.com>
2026-08-19 17:43:50 +0200

    supp liens

--- app/page.tsx
+++ app/page.tsx
@@ -257,38 +257,14 @@ const resultRef = useRef<HTMLDivElement | null>(null);
 </p>
 <div className="mt-8">
   <h2 className="text-lg font-bold text-white">
-    👥 Communauté WeWard
+    Pas d'jaloux ! Y'en a pour tous le monde ! J'ai pas crée ça pour avoir de la jalousie juste pour aider !
   </h2>
 
   <p className="mt-1 text-sm text-slate-400">
-    Retrouvez la communauté sur Facebook.
+   Flagada vous remercie ! Et si ca vous plait pas c'est pareil. Amicalement votre
   </p>
 
-<div className="mt-4 flex items-center justify-center gap-8"> <a
-  href="https://www.facebook.com/groups/1539718463416164/"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <img
-    src="/facebook-groupe-1.jpg"
-    alt="Groupe Facebook 1"
-className="h-20 w-20 rounded-2xl object-cover"
-  />
-</a>
 
-    <a
-  href="https://www.facebook.com/groups/2423817841302793/"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  <img
-    src="/facebook-groupe-2.jpg"
-    alt="Groupe Facebook 2"
-className="h-20 w-20 rounded-2xl object-cover"
-  />
-</a>
-  </div>
-</div>
 {/*
 <p className="mt-5 text-sm text-slate-400">
   Vous pensez qu’un profil devrait être signalé ?
